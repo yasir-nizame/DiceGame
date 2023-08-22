35daaa13e20228462d9548af9ec82152c0431b6f
yasir-nizame <130412330+yasir-nizame@users.noreply.github.com>
2023-08-22 18:22:48 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+
+var dice1 = "/Dicee Challenge - Starting Files/images/dice" + randomNumber1 +".png";
+var dice2 = "/Dicee Challenge - Starting Files/images/dice" + randomNumber2 +".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", dice1);
+document.querySelectorAll("img")[1].setAttribute("src", dice2);
+
+ if(randomNumber1>randomNumber2){
+     document.querySelectorAll("h1")[0].innerHTML=" 🚩Player 1 wins !";
+ }
+
+ else if(randomNumber1<randomNumber2){
+     document.querySelectorAll("h1")[0].innerHTML=" 🚩Player 2 wins !";
+ }
+
+ else{
+     document.querySelectorAll("h1")[0].innerHTML="⚔️ Draw !";
+
+ }
