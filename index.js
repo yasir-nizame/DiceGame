3b0fb27f394020f2c976fa8eb781138b5738b80e
yasir-nizame <130412330+yasir-nizame@users.noreply.github.com>
2023-08-24 17:28:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-var randomNumber1=Math.floor(Math.random()*6)+1;
+ var randomNumber1=Math.floor(Math.random()*6)+1;
 
 
-var randomNumber2=Math.floor(Math.random()*6)+1;
+ var randomNumber2=Math.floor(Math.random()*6)+1;
 
-var dice1 = "/Dicee Challenge - Starting Files/images/dice" + randomNumber1 +".png";
-var dice2 = "/Dicee Challenge - Starting Files/images/dice" + randomNumber2 +".png";
+ var dice1 = "/images/dice" + randomNumber1 +".png";
+ var dice2 = "/images/dice" + randomNumber2 +".png";
 
-document.querySelectorAll("img")[0].setAttribute("src", dice1);
-document.querySelectorAll("img")[1].setAttribute("src", dice2);
+ document.querySelectorAll("img")[0].setAttribute("src", dice1);
+ document.querySelectorAll("img")[1].setAttribute("src", dice2);
 
- if(randomNumber1>randomNumber2){
-     document.querySelectorAll("h1")[0].innerHTML=" 🚩Player 1 wins !";
- }
+  if(randomNumber1>randomNumber2){
+      document.querySelectorAll("h1")[0].innerHTML=" 🚩Player 1 wins !";
+  }
 
- else if(randomNumber1<randomNumber2){
-     document.querySelectorAll("h1")[0].innerHTML=" 🚩Player 2 wins !";
- }
+  else if(randomNumber1<randomNumber2){
+      document.querySelectorAll("h1")[0].innerHTML=" 🚩Player 2 wins !";
+  }
 
- else{
-     document.querySelectorAll("h1")[0].innerHTML="⚔️ Draw !";
+  else{
+      document.querySelectorAll("h1")[0].innerHTML="⚔️ Draw !";
 
- }
+  }
